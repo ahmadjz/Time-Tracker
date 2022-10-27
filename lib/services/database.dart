66baccee0f9aa97088596049ecd1ada8c27c47bc
Ahmad jz <ahmadjzz99@gmail.com>
2022-10-27 13:30:43 +0300

    link with Firestore database and create its methods

---
+++ lib/services/database.dart
@@ -0,0 +1,27 @@
+import 'package:time_tracker/app/home/models/job.dart';
+import 'package:time_tracker/services/api_path.dart';
+import 'package:time_tracker/services/firestore_service.dart';
+
+abstract class Database {
+  Future<void> setJob(Job job);
+  Stream<List<Job>> jobsStream();
+}
+
+String documentIdFromCurrentDate() => DateTime.now().toIso8601String();
+
+class FirestoreDatabase implements Database {
+  FirestoreDatabase({required this.uid});
+  final String uid;
+
+  final _service = FirestoreService.instance;
+
+  @override
+  Future<void> setJob(Job job) async =>
+      _service.setData(path: APIPath.job(uid, job.id), data: job.toMap());
+
+  @override
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
+        path: APIPath.jobs(uid),
+        builder: (data) => Job.fromMap(data.data(), data.id),
+      );
+}
---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  FirestoreService._();
+  static final instance = FirestoreService
+      ._(); // this two lines is to make a singleton class se we make only on instance of it, by this we can't call this class without .instance
+
+  final _service = FirebaseFirestore.instance;
+
+  Stream<List<T>> collectionStream<T>(
+      {required String path,
+      required T Function(QueryDocumentSnapshot<Map<String, dynamic>> data)
+          builder}) {
+    final reference = _service.collection(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map((snapshot) => snapshot.docs
+        .map(
+          (snapshot) => builder(snapshot),
+        )
+        .toList());
+  }
+
+  Future<void> setData(
+      {required String path, required Map<String, dynamic> data}) async {
+    final documentReference = _service.doc(path);
+    documentReference.set(data);
+  }
+}
